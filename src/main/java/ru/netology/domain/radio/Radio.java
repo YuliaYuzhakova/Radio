f7cbcfe1f04858d06f2ce2527ff56950ee6cf127
YuliaYuzhakova <JuliaU33@mail.ru>
2022-05-31 15:51:35 +0700

    Добавлены методы&тесты для выбора станций Radio

--- src/main/java/ru/netology/domain/radio/Radio.java
+++ src/main/java/ru/netology/domain/radio/Radio.java
@@ -1,4 +1,44 @@
 package ru.netology.domain.radio;
 
 public class Radio {
+
+    private int currentStation;
+    public int soundVolume;
+
+
+    //Возможность выставлять номер радиостанции через прямое указание её номера, но в рамках 0 - 9
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+            return;
+        }
+        if (newCurrentStation > 9) {
+            return;
+        }
+        this.currentStation = newCurrentStation;
+    }
+
+    //Выбор следующей станции Next
+    public int NextStation(int newCurrentStation) {
+        if (newCurrentStation >= 9) {
+            currentStation = 0;
+        }
+        if (newCurrentStation < 9) {
+            currentStation = newCurrentStation + 1;
+        }
+        return currentStation;
+    }
+
+    //Выбор предыдущей станции Prev
+    public void PrevStation(int newCurrentStation) {
+        if (newCurrentStation <= 0) {
+            currentStation = 9;
+        }
+        if (newCurrentStation > 0) {
+            currentStation = newCurrentStation - 1;
+        }
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
 }
--- src/test/java/ru/netology/domain/radio/RadioTest.java
+++ src/test/java/ru/netology/domain/radio/RadioTest.java
@@ -1,4 +1,109 @@
 package ru.netology.domain.radio;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RadioTest {
+
+    //Возможность выставлять номер радиостанции через прямое указание её номера, но в рамках 0 - 9
+    //Проверка значений 10, 5, -1
+    @Test
+    void choiceRadioStation() {
+        Radio station = new Radio();
+
+        station.setCurrentStation(5);
+
+        int actual = station.getCurrentStation();
+        int expected = 5;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void choiceRadioStationMax() {
+        Radio station = new Radio();
+
+        station.setCurrentStation(10);
+
+        int actual = station.getCurrentStation();
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void choiceRadioStationMin() {
+        Radio station = new Radio();
+
+        station.setCurrentStation(-1);
+
+        int actual = station.getCurrentStation();
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    //Выбор следующей станции Next, проверка граничных значений 10, 9, 8
+    @Test
+    void choiceNextStation1() {
+        Radio station = new Radio();
+        station.NextStation(10);
+        int actual = station.getCurrentStation();
+        int expected = 0;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void choiceNextStation2() {
+        Radio station = new Radio();
+        station.NextStation(8);
+        int actual = station.getCurrentStation();
+        int expected = 9;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void choiceNextStation3() {
+        Radio station = new Radio();
+        station.NextStation(9);
+        int actual = station.getCurrentStation();
+        int expected = 0;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    //Выбор предыдущей станции Prev, , проверка граничных значений 2, 0, -1
+    @Test
+    void choicePrevStation1() {
+        Radio station = new Radio();
+        station.PrevStation(2);
+        int actual = station.getCurrentStation();
+        int expected = 1;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void choicePrevStation2() {
+        Radio station = new Radio();
+        station.PrevStation(0);
+        int actual = station.getCurrentStation();
+        int expected = 9;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void choicePrevStation3() {
+        Radio station = new Radio();
+        station.PrevStation(-1);
+        int actual = station.getCurrentStation();
+        int expected = 9;
+
+        Assertions.assertEquals(actual, expected);
+    }
 }
